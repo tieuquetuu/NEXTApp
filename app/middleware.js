2c3a8ed8845f64bffe4970501b984e19fbd643af
tieuquetuu <anhvanvan1102@gmail.com>
2018-12-19 01:39:28 +0700

    update

---
+++ app/middleware.js
@@ -0,0 +1,18 @@
+/**
+ *  App Check Middleware
+ */
+
+// Dependencies
+
+
+// Container for all the middleware
+var lib = {};
+
+// App check the token in headers route
+lib.sessionChecked = function (data, callback) {
+    console.log(123);
+};
+
+
+// Export the module
+module.exports = lib;
--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -2,12 +2,45 @@
  * You must include the dependency on 'ngMaterial'
  */
 
-var app = angular.module('BlankApp', ['ngMaterial', 'ngMessages']);
+var app = angular.module('BlankApp', ['ngMaterial', 'ngMessages'])
 
 // Get The Current User
 var globalsConfig = {
     currentUser : false
 };
+app.constant("moment", moment);
+app.config(function($mdDateLocaleProvider, moment) {
+    /**
+     * @param date {Date}
+     * @returns {string} string representation of the provided date
+     */
+    $mdDateLocaleProvider.formatDate = function(date) {
+        return date ? moment(date).format('M/D') : '';
+    };
+
+    /**
+     * @param dateString {string} string that can be converted to a Date
+     * @returns {Date} JavaScript Date object created from the provided dateString
+     */
+    $mdDateLocaleProvider.parseDate = function(dateString) {
+        var m = moment(dateString, 'M/D', true);
+        return m.isValid() ? m.toDate() : new Date(NaN);
+    };
+
+    /**
+     * Check if the date string is complete enough to parse. This avoids calls to parseDate
+     * when the user has only typed in the first digit or two of the date.
+     * Allow only a day and month to be specified.
+     * @param dateString {string} date string to evaluate for parsing
+     * @returns {boolean} true if the date string is complete enough to be parsed
+     */
+    $mdDateLocaleProvider.isDateComplete = function(dateString) {
+        dateString = dateString.trim();
+        // Look for two chunks of content (either numbers or text) separated by delimiters.
+        var re = /^(([a-zA-Z]{3,}|[0-9]{1,4})([ .,]+|[/-]))([a-zA-Z]{3,}|[0-9]{1,4})/;
+        return re.test(dateString);
+    };
+});
 
 app.factory('appHttp', ["$http", function ($http) {
     var _lib = {};
@@ -29,7 +62,7 @@ app.factory('appHttp', ["$http", function ($http) {
 
     return _lib;
 }]);
-app.controller('AppBodyCtrl', ["$scope" , "appHttp",function ($scope , appHttp, $timeout, $mdSidenav, $log) {
+app.controller('AppBodyCtrl', function ($scope , appHttp, $timeout, $mdSidenav, $log) {
     $scope.avatarUrl = 'public/img/avatar/male.png';
     $scope.toggleLeft = buildDelayedToggler('left');
     $scope.toggleRight = buildToggler('right');
@@ -118,7 +151,7 @@ app.controller('AppBodyCtrl', ["$scope" , "appHttp",function ($scope , appHttp, 
 
         });
     };
-}])
+})
     .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
         $scope.close = function () {
             // Component lookup should always be available since we are not using `ng-if`
@@ -152,6 +185,20 @@ app.controller('AppBodyCtrl', ["$scope" , "appHttp",function ($scope , appHttp, 
                 icon: "person"
             }
         ];
+    })
+    .controller('CheckListCtrl', function ($scope, $log) {
+        $scope.todos = [
+            { name: 'Pepperoni', isDone: true },
+            { name: 'Sausage', isDone: false },
+            { name: 'Black Olives', isDone: true },
+            { name: 'Green Peppers', isDone: false }
+        ];
+
+        this.myDate = new Date();
+
+        this.onDateChanged = function() {
+            $log.log('Updated Date: ', this.myDate);
+        };
     })
     .controller('ProfileCtrl', [ "$scope", "appHttp",function ($scope, appHttp) {
 
